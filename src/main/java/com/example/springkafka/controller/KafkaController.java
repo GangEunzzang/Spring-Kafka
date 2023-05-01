fa47ff7b81ceef7600e88deb1c2cdfea49898b1a
이강은 <rkddms123456@gmail.com>
2023-05-01 23:17:20 +0900

    Initial commit

---
+++ src/main/java/com/example/springkafka/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package com.example.springkafka.controller;
+
+import com.example.springkafka.service.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final KafkaProducer kafkaProducer;
+
+    @PostMapping
+    public String sendMessage(@RequestParam("message") String message) {
+        kafkaProducer.sendMessage(message);
+        return "success";
+    }
+}
